62dcec3437afe35efd7a08ec8d132e544891e2e1
Luca8991 <luca.berte98@hotmail.it>
2023-07-27 20:42:24 +0200

    fix: export default in index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
+// import default member
+import IcWebSocket from './ic-websocket';
+// re-export non-default members
 export * from './ic-websocket';
+// re-export default member
+export default IcWebSocket;
